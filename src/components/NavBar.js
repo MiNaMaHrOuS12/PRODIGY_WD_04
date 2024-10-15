26974951bd37819c3665aa7f911eea3fb717f43d
Mina Mahrous <minamahrous524@gmail.com>
2024-10-15 20:13:20 +0300

    Image Instalation and Hero section done + navbar modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,8 +9,22 @@ const NavBar = () => {
 
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
+
+    const [scroll,setScroll] = useState(false)
+    
+    const changeColor = () => {
+      if (window.scrollY >= 100)
+      {
+        setScroll(true);
+      }else
+      {
+        setScroll(false);
+      }
+    }
+
+    window.addEventListener("scroll", changeColor);
   return (
-    <div className="Header">
+    <div className={scroll ? "Header Header-bg" : "Header"}>
       <Link to='/'>
         <h1>Portfolio</h1>
       </Link>
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Project = () => {
   return (
     <div>
-      
+      <h1>Projects</h1>
     </div>
   )
 }
