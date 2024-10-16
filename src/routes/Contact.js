065577bf9b94a0819bdc3fcb60ef5acca3c21df1
Mina Mahrous <minamahrous524@gmail.com>
2024-10-16 16:27:10 +0300

    Created About section + Contact Form. (Portfolio Done!)

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,13 @@ import React from 'react'
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer"
 import HeroSection2 from '../components/HeroSection2';
-
+import Form from "../components/Form"
 const Contact = () => {
   return (
     <div>
       <NavBar />
       <HeroSection2 heading="CONTACT." text="Let's have a chat"/>
+      <Form />
       <Footer />
     </div>
   )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,13 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import Footer from "../components/Footer"
-
+import Work from "../components/Work" 
 const Home = () => {
     return (
         <div>
             <NavBar />
             <HeroSection />
+            <Work />
             <Footer />
         </div>
     );
